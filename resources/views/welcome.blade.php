40b80822b257a24cc14028d8b0cbb609463ae85d
Ian at work <ian.mclean@igovtt.tt>
2020-07-23 21:30:18 -0400

    styling to welcomepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,10 @@
 @section('content')
     <div class="justify-content-center py-4">
         <div class="col-lg-10 col-md-12 offset-lg-1">
+            <div class="col-xl-4 offset-xl-4 col-sm-6 offset-sm-3 col-xs-10 offset-xs-1 mb-2">
+                <img class="mx-auto d-block" src="{{asset('img/msdfs_logo.png')}}" alt="MSDFS Logo" width="70%">
+            </div>
+
             <div class="card border-primary shadow">
                 <div class="card-body">
                     <h4 class="mb-4 text-">
