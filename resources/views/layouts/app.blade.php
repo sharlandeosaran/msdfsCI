c6496d7b393203bc0f030fb0a01edb5ac11cd333
Ian at work <ian.mclean@igovtt.tt>
2020-07-24 12:58:18 -0400

    updated favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/png" href="{{url('/img/TTCrest.png')}}" />
+    <link rel="icon" type="image/png" href="{{url('/img/favicon.png')}}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
